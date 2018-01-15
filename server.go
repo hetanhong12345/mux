fe012531f51f5c6481ba6375cc74861909f4a839
hetanhong <543039822@qq.com>
2018-01-15 18:59:31 +0800

    add handler func

--- server.go
+++ server.go
@@ -12,6 +12,8 @@ func main() {
 	n := negroni.Classic()
 	// 添加全局handler
 	n.UseHandler(handler())
+	// 添加全局handlerFunc
+	n.UseHandlerFunc(handlerFunc)
 	// 挂载根路由
 	n.UseHandler(router.Routes)
 	// 监听端口
@@ -19,12 +21,19 @@ func main() {
 }
 
 /*
-  全局中间件
+  全局中间件 handler
 */
 func handler() http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Println("<----middleware----->")
+		fmt.Println("<----middleware---handler-->")
 		fmt.Println(r.Header.Get("User-Agent"))
 
 	})
+}
+
+/*
+  全局中间件 handlerFunc
+*/
+func handlerFunc(rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("<----middleware---handlerFunc-->")
 }
