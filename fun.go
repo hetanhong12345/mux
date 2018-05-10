3631f6129afa91784a064dbd03928db184d78eed
hetanhong <543039822@qq.com>
2018-05-10 10:52:57 +0800

    change project name

--- fun.go
+++ fun.go
@@ -258,20 +258,3 @@ func zuhe(m, n int) int {
 
 }
 
-func maxSubArray(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	max, sum := 0, 0
-	for _, v := range nums[1:] {
-		sum += v
-		if sum > 0 {
-			max = sum
-		}
-		if sum < 0 {
-			sum = 0
-		}
-
-	}
-	return max
-}
